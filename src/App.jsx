00e9a501fa2a7d0980be567701eed86d91d81808
Aldahir <aldahir0603@hotmail.com>
2025-12-25 10:43:34 -0600

    Agregar un input y usando state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react'
 import Header from "./components/Header"
 
 function App() {
+  let [cantidad, setCantidad] = useState(10000);
+
+  console.log(cantidad);
+
+  function handleChange(e) {
+    console.log(Number(e.target.value));
+  }
 
   return (
       <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
-        
         <Header />
+
+        <input 
+          type="range" 
+          className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+          onChange={ handleChange }
+        />
       </div>
   )
 }
