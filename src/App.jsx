4607e260348f1636d6980f9af2bc69d99ac0e1f9
Aldahir <aldahir0603@hotmail.com>
2025-12-25 01:34:25 -0600

    Definiendo Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import Header from "./components/Header"
+
 function App() {
-
-  const hola = "Hola Mundo";
-
-  const auth = false;
 
   return (
       <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
-        {auth ? 'autenticado' : 'no autenticado'}
+        
+        <Header />
       </div>
   )
 }
