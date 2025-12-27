d49160f0c05701915541a4625e68b1a834db0c5b
Aldahir <aldahir0603@hotmail.com>
2025-12-27 15:38:04 -0600

    Añadiendo Input, Botones y States.

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,71 @@ import { useState } from 'react'
 import Header from "./components/Header"
 
 function App() {
-  let [cantidad, setCantidad] = useState(10000);
+  const [cantidad, setCantidad] = useState(10000);
 
-  console.log(cantidad);
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100;
 
   function handleChange(e) {
-    console.log(Number(e.target.value));
+    setCantidad(+e.target.value);
+  }
+
+  function handleClickDecremento() {
+    const valor = cantidad - STEP;
+
+    if(valor < MIN) {
+      alert("Cantidad no válida.")
+      return;
+    }
+
+    setCantidad(valor);
+  }
+
+  function handleClickIncremento() {
+    const valor = cantidad + STEP;
+
+    if(valor > MAX) {
+      alert("Cantidad no válida.")
+      return;
+    }
+
+    setCantidad(valor);
   }
 
   return (
       <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
         <Header />
 
+        <div className='flex justify-between my-6'>
+          <button
+            type='button'
+            className='h-10 w-10 flex items-center justify-center font-bold text-white
+            text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2
+            hover:ring-offset-2 hover:ring-lime-500'
+            onClick={handleClickDecremento}
+          >-</button>
+
+          <button
+            type='button'
+            className='h-10 w-10 flex items-center justify-center font-bold text-white
+            text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2
+            hover:ring-offset-2 hover:ring-lime-500'
+            onClick={handleClickIncremento}
+          >+</button>
+        </div>
+
         <input 
           type="range" 
           className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
           onChange={ handleChange }
+          min={0}
+          max={20000}
+          step={100}
+          value={cantidad}
         />
+
+        <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>{cantidad}</p>
       </div>
   )
 }
